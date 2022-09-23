51eb99d65bf66f8cb7e0c5c2b9e60aefefee1fe7
Aman Yadav <95525722+amanycodes@users.noreply.github.com>
2022-09-23 21:44:50 +0530

    Update homepage.js

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -5,29 +5,29 @@ let middle = document.getElementById("middle")
 let front = document.getElementById("front")
 let text = document.getElementById("text")
 let static = document.getElementById("static")
-let value = this.window.scrollY
 let h2 = document.querySelector(".h2")
+let value = this.window.scrollY
 window.addEventListener('scroll', function(){
     let value = this.window.scrollY
-    stars.style.left = value * 0.25 + 'px'
+    stars.style.left = value * 0.5 + 'px'
     static.style.top = value * 1 + 'px'
     fore.style.top = value * .75 + 'px'
     back.style.top = value * .5 + 'px'
     middle.style.top = value * .25 + 'px'
     front.style.top = value * .125 + 'px'
-    
     if(value < 300){
         text.style.marginLeft = value * 10 + 'px'
         text.style.zIndex = '9'
+        
     }else if(value>=300 && value <490){
         text.style.fontSize = (7.5 - (value - 300)/100) + 'vw'
         text.style.marginTop = (value-300)* 2.2 +'px'
         text.style.zIndex = '10'
-    }else if(value >=490 && value < 703.5){
+    }else if(value >=490 && value < 710){
         text.style.marginTop = (value-300)* 2.2 +'px'
         text.style.zIndex = '12'
-    }else if(value >= 703.5 && value < 750){
-        text.style.marginRight = ((value-703.5)*7) + 'px'
+    }else if(value >= 710 && value < 750){
+        text.style.marginRight = ((value-710)*7) + 'px'
         text.style.zIndex = '12'
         h2.classList.add("auto-commplete", "about")
         h2.classList.remove("before")
@@ -37,7 +37,7 @@ window.addEventListener('scroll', function(){
     }
 })
 var typed = new Typed(".auto-complete", {
-    strings: ["    ", "About", "What is"],
+    strings: ["    ", "About", "What is" , "About","What is", "About" ,"What is", "About","What is", "About", "What is","About","What is", "About"],
     
     typeSpeed: 100,
     backSpeed: 100,
